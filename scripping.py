40afb04ca13557f4fbc5821ad9406743d6788b1a
P.Jay <azilakujeremiah@gmail.com>
2022-01-06 03:58:35 +0000

    first commit

---
+++ scripping.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+with open('simple.html', mode = 'r') as html_file:
+    soup = BeautifulSoup(html_file,'lxml')
+
+
+for article in soup.find_all('div', class_ = 'article'):
+
+    headline = article.h2.a.text
+    summary = article.p.text
+
+
+    print(headline)
+    print(summary)
+
+    print()
+
